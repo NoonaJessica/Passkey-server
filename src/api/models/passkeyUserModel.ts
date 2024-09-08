4660e884959bde6b563bd18049dce68e37e2fc3d
Noona <noonaposti@gmail.com>
2024-09-08 19:52:20 +0300

    first commit

--- src/api/models/passkeyUserModel.ts
+++ src/api/models/passkeyUserModel.ts
@@ -3,6 +3,13 @@ import {PasskeyUserPost} from '../../types/PasskeyTypes';
 
 const PasskeyUserSchema = new Schema<PasskeyUserPost>({
   // TODO: add userId (Number, required, unique)
+  userId:{type: Number, required: true, unique: true},
+  email:{type: String, required: true, unique: true},
+  devices:{
+    type: [Schema.Types.ObjectId],
+    required: true,
+    ref: 'AuthenticatorDevice'
+  },
   // TODO: add email (String, required, unique)
   // TODO: add devices (Array of ObjectIds, required, ref: 'AuthenticatorDevice')
 });
